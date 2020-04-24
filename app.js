c35dfaec3b7d4c73be5bfbed42d5a8830c0850bd
Leslie <lturis@gmail.com>
2020-04-24 15:17:59 -0400

    add /test route w custom header

--- app.js
+++ app.js
@@ -5,5 +5,8 @@ const PORT = 3000;
 app.get('/', (req, res) => {
 	res.send('HELLO world');
 });
+app.get('/test', (req, res) => {
+	res.status(418).send('YASS I WORK')
+})
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}~`));
